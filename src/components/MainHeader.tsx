ab3b569223ab84decb3e2ae5589b4c9daaec91f0
The Fox <lisieckikamil93@gmail.com>
2023-12-08 20:36:43 +0100

    feat(manage-ingredients-ui): add MainHeader component
    - Created MainHeader component for managing ingredients UI
    - This component will be used for the top navigation in the ingredients management section

---
+++ src/components/MainHeader.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const MainHeader = () => {
+  return <Container></Container>;
+};
+
+export default MainHeader;
