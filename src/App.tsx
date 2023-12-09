7d93a7ccab1a07e2ea9306ccb4a7e2a6d6dcca86
The Fox <lisieckikamil93@gmail.com>
2023-12-09 02:40:23 +0100

    fix(problem with ai review)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import GlobalStyle from "./GlobalStyles";
-// import DumplingGenerator from "./components/DumlingGenerator";
 import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <>
-      {/* <DumplingGenerator /> */}
       <MainHeader />
       <GlobalStyle />
     </>
