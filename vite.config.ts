f396446f8b93bb315fc9cfc7ab6281b331fa6359
Szymon Kadaś <kadasszymon@gmail.com>
2023-12-11 20:56:58 +0100

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,5 @@ export default defineConfig({
   server: {
     host: "0.0.0.0", // Dodaj tę linię
   },
+  base: "/pierogator-app-lpwyyiux/",
 });
